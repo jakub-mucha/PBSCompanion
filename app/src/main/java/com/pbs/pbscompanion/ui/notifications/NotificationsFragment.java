09dfe7ce19ee2471c2a59f9e7c085df984010b3e
Jakub Mucha <jakmuc000@pbs.edu.pl>
2022-01-29 19:56:03 +0100

    Added support for news database. Added view for displaying the news. Added the ability to filter news by department.

--- app/src/main/java/com/pbs/pbscompanion/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/pbs/pbscompanion/ui/notifications/NotificationsFragment.java
@@ -1,9 +1,16 @@
 package com.pbs.pbscompanion.ui.notifications;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,7 +18,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.pbs.pbscompanion.DAO;
 import com.pbs.pbscompanion.R;
 import com.pbs.pbscompanion.databinding.FragmentNotificationsBinding;
 
@@ -19,6 +29,11 @@ public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
+    private DAO dao;
+    private SQLiteDatabase newsDB;
+    private RadioGroup radioDepartmentGroup;
+    private RadioButton radioDepartmentButton;
+    private Button btnDisplay;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,13 +43,57 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        dao = new DAO();
+        dao.initializeDB();
+        newsDB = dao.getNewsDB();
+        TableLayout tableLayout = (TableLayout) root.findViewById(R.id.newsTable);
+
+        radioDepartmentGroup=(RadioGroup) root.findViewById(R.id.department);
+        btnDisplay=(Button) root.findViewById(R.id.select);
+        btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                int selectedId= radioDepartmentGroup.getCheckedRadioButtonId();
+                radioDepartmentButton= (RadioButton) root.findViewById(selectedId);
+                String department = (String) radioDepartmentButton.getText();
+                tableLayout.removeAllViews();
+                Cursor cursor = newsDB.rawQuery("SELECT * FROM news WHERE department LIKE '" + department + "';", null);
+                cursor.moveToFirst();
+                for (int i = 0; i < cursor.getCount(); i++)
+                {
+                    TableRow tableRow = new TableRow(v.getContext());
+                    TextView text = new TextView(v.getContext());
+                    text.setText(cursor.getString(1));
+                    tableRow.addView(text);
+                    tableLayout.addView(tableRow);
+
+                    tableRow = new TableRow(v.getContext());
+                    text = new TextView(v.getContext());
+                    text.setText(cursor.getString(4));
+                    tableRow.addView(text);
+                    tableLayout.addView(tableRow);
+
+                    tableRow = new TableRow(v.getContext());
+                    text = new TextView(v.getContext());
+                    text.setText(cursor.getString(2));
+                    tableRow.addView(text);
+                    tableLayout.addView(tableRow);
+
+                    text = new TextView(v.getContext());
+                    text.setText("");
+                    tableRow = new TableRow(v.getContext());
+                    tableRow.addView(text);
+
+                    tableLayout.addView(tableRow);
+
+                    cursor.moveToNext();
+                }
             }
         });
+
+
+
+
         return root;
     }
 
@@ -42,5 +101,9 @@ public class NotificationsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void displayNews() {
+
     }
 }
--- app/src/main/java/com/pbs/pbscompanion/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/pbs/pbscompanion/ui/notifications/NotificationsViewModel.java
@@ -1,16 +1,22 @@
 package com.pbs.pbscompanion.ui.notifications;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.pbs.pbscompanion.DAO;
 
 public class NotificationsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
+
     public NotificationsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        //mText = new MutableLiveData<>();
+        //mText.setValue(cursor.getString(1));
     }
 
     public LiveData<String> getText() {
